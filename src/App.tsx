43bd508f3e7c2c13773487e7d64b4557ce80aee0
nobuhiro naito <you@example.com>
2022-01-14 21:47:33 +0900

    era

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const App:React.FC = () => {
-  return <div className='App'></div>;
+const App: React.FC = () => {
+  return <div className='App'></div>
+  <h1>hohoho</h1>
 };
 
 export default App;
+
